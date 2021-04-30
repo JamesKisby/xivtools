62291a70bcd18f6ae44f1cfbae6b4b8547fb3588
James Kisby <james.kisby@hotmail.com>
2021-04-29 21:31:06 -0400

    quickmarch iframe added

--- xivtools.web/src/js/components/Canvas.jsx
+++ xivtools.web/src/js/components/Canvas.jsx
@@ -6,20 +6,10 @@ import Typography from "@material-ui/core/Typography";
 export default function Canvas({ match }) {
   const url = "https://xivtools.com/training/" + match.params.raidname + "/" + match.params.mechanic + "/index.html";
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = "/src/js/training/" + match.params.raidname + "/" + match.params.mechanic + ".js";
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, [match.params.mechanic]);
-
 
   return (
     <div>
+      <iframe src={url} width="500" height="500" ></iframe>
     </div>
   );
 }
